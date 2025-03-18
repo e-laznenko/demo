67f45c1e08f1689e031fdff210d1a7ec01f0a805
Kateryna Laznenko <laznenko@gmail.com>
2025-03-17 20:40:02 -0400

    task02: changes status code

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -10,12 +10,17 @@ exports.handler = async (event) => {
   if (method === "GET" && path === "/hello") {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello from Lambda" }),
+      body: JSON.stringify({
+        statusCode: 200,
+        message: "Hello from Lambda"
+     }),
     };
   }
 
     return {
-        statusCode: 400,
-        body: JSON.stringify({ message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}` })
+        statusCode: 200,
+        body: JSON.stringify({ 
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}` })
     };
 };
